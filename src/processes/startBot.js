06fda49c82ce57280c9ef88db0146f7676eabfd6
mcpeblocker <mcpeblocker@gmail.com>
2021-10-01 15:59:04 +0500

    default production mode

--- src/processes/startBot.js
+++ src/processes/startBot.js
@@ -2,29 +2,29 @@ const express = require('express');
 const config = require('../config');
 
 module.exports = (bot) => {
-    if (config.nodeEnv === 'production' || config.nodeEnv === 'prod') {
-        const secretPath = `/telegraf/${bot.secretPathComponent()}`;
-        bot.telegram.setWebhook(`${config.host}${secretPath}`);
-        const app = express();
-        app.get('/', (req, res) => { res.send('Hello world') });
-        app.use(bot.webhookCallback(secretPath));
-        app.listen(config.port, () => {
-            console.log('App is listening at port ' + config.port);
+    // if (config.nodeEnv === 'production' || config.nodeEnv === 'prod') {
+    //     const secretPath = `/telegraf/${bot.secretPathComponent()}`;
+    //     bot.telegram.setWebhook(`${config.host}${secretPath}`);
+    //     const app = express();
+    //     app.get('/', (req, res) => { res.send('Hello world') });
+    //     app.use(bot.webhookCallback(secretPath));
+    //     app.listen(config.port, () => {
+    //         console.log('App is listening at port ' + config.port);
+    //     })
+    // } else {
+    bot.telegram.deleteWebhook()
+        .then(() => {
+            console.log('Webhook cleared');
+            bot.launch()
+                .then(() => {
+                    console.log(`@${bot.botInfo.username} started`);
+                })
+                .catch((err) => {
+                    throw err;
+                });
         })
-    } else {
-        bot.telegram.deleteWebhook()
-            .then(() => {
-                console.log('Webhook cleared');
-                bot.launch()
-                    .then(() => {
-                        console.log(`@${bot.botInfo.username} started`);
-                    })
-                    .catch((err) => {
-                        throw err;
-                    });
-            })
-            .catch((err) => {
-                console.error(`Can't clear webhook: ${err}`);
-            })
-    }
+        .catch((err) => {
+            console.error(`Can't clear webhook: ${err}`);
+        })
+    // }
 };
